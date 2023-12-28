1ebc9f60f85521cc3a8f28f3f171dbfd5d02df2b
Ori <uic3883@gmail.com>
2023-12-28 15:05:13 +0900

    적 처치 애니메이션

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -13,27 +14,37 @@ public class Enemy : MonoBehaviour
     bool isLive; // 생존여부
 
     Rigidbody2D rigid;
+    Collider2D coll;
     SpriteRenderer spriter;
     Animator anim;
+    WaitForFixedUpdate wait;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
         spriter = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        wait = new WaitForFixedUpdate();
     }
 
     void OnEnable()
     {
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+
         // 오브젝트 활성화 시, 초기 설정을 적용
         isLive = true;
         health = maxHealth;
+        coll.enabled = true;
+        rigid.simulated = true;
+        spriter.sortingOrder = 2;
+        anim.SetBool("Dead", false);
+
     }
 
     void FixedUpdate()
     {
-        if (!isLive)
+        if (!isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit"))
             return;
 
         // 위치 차이 = 타겟 위치 - 나의 위치
@@ -60,26 +71,55 @@ public class Enemy : MonoBehaviour
         health = data.health;
     }
 
+    // 애니메이션 이벤트를 통해 호출
     void Dead()
     {
         gameObject.SetActive(false);
     }
 
+    IEnumerator KnockBack()
+    {
+        yield return wait; //  다음 하나의 물리 프레임 딜레이
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        // 플레이어 기준의 반대 방향 : 현재위치 - 플레이어 위치
+        Vector3 dirVec = transform.position - playerPos;
+
+        // 넉백 (순간적인 힘이므로 ForceMode2D.Impulse 사용)
+        rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Bullet"))
+        if (!collision.CompareTag("Bullet") || !isLive)
             return;
 
         health -= collision.GetComponent<Bullet>().damage;
+        StartCoroutine(KnockBack());
 
         if(health > 0)
         {
-            // Live, Hit Action
+            // 피격 애니메이션
+            anim.SetTrigger("Hit");
         }
         else
         {
             // Die
-            Dead();
+            isLive = false;
+            coll.enabled = false;
+            rigid.simulated = false;
+
+            // 오더 레이어 다운
+            spriter.sortingOrder = 1;
+
+            // 애니메이션
+            anim.SetBool("Dead", true);
+
+            // Die 함수는 애니메이션의 이벤트에서 설정
+
+            // 게임매니저 정보 추가
+            GameManager.instance.kill++;
+            GameManager.instance.GetExp();
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Player player;
-    public PoolManager pool;
+    [Header("# Player Info")] // 인스펙터의 속성들을 이쁘게 구분시켜주는 타이틀
+    public int level;
+    public int kill;
+    public int exp;
+    public int[] nextExp = { 10, 30, 60, 100, 150, 210, 280, 360, 450, 600 }; // 각 레벨의 필요 경험치를 보관할 배열 변수 선언 및 초기화
 
+    [Header("# Game Control")]
     public float gameTime; // 게임시간
     public float maxGameTime = 2 * 10f; // 최대게임시간
+
+    [Header("# Game Object")]
+    public Player player;
+    public PoolManager pool;
     
     // 정적으로 사용하겠다는 키워드. 바로 메모리에 얹어버림.
     // 초기화 없이 다른 코드에서 사용 가능 (앞에 자료형은 붙여줘야 함)
@@ -27,6 +35,17 @@ public class GameManager : MonoBehaviour
         if(gameTime >= maxGameTime)
         {
             gameTime = maxGameTime;
+        }
+    }
+
+    public void GetExp()
+    {
+        exp++;
+
+        if(exp == nextExp[level])
+        {
+            level++;
+            exp = 0;
         }
     }
 }
